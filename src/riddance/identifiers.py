890e35d0138fb3ae6d09bef10711ddbae07ba2d3
rylan-justice <119366377+rylan-justice@users.noreply.github.com>
2024-02-16 20:29:32 -0500

    Revise docstring

--- src/riddance/identifiers.py
+++ src/riddance/identifiers.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with riddance.  If not, see <https://www.gnu.org/licenses/>.
 
-"""Compatible operating systems and versions."""
+"""Compatible operating systems and their versions."""
 
 os_info = {
     "Linux": {
